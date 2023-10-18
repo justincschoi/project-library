87cff1c4d98d58a654c9d53c3e43132eb7742b16
justincschoi <justincschoi@gmail.com>
2023-10-17 20:09:09 -0400

    Submit button enters book into myLibrary

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const myLibrary = [];
 const addButton = document.querySelector('.add-button');
 const bookContainer = document.querySelector('.book-container');
 const closeButton = document.querySelector('.close');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
+const submitButton = document.querySelector('.submit');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -25,3 +30,24 @@ function hideBookContainer() {
 
 addButton.addEventListener('click', showBookContainer);
 closeButton.addEventListener('click', hideBookContainer);
+submitButton.addEventListener('click', function () {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = parseInt(pagesInput.value);
+    const read = readInput.checked;
+
+    if (!title || !author || isNaN(pages)) {
+        alert("Please fill in all the required fields.");
+        return;
+    }
+
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    readInput.checked = false;
+
+    hideBookContainer();
+});
